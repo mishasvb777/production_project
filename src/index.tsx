2f6a492d9aad641f6c0160b5f637ea80716bcfaf
mishasvb777 <mishasvb777@gmail.com>
2023-10-17 20:17:07 +0300

    Button

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import App from './app/App'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 import 'shared/config/i18n/i18n'
 import { ErrorBoundary } from 'app/providers/ErrorBoundaruy'
+import Button from 'shared/ui/Button/Button'
 
 // Оборачиваем все прилоджения в BrowserRouter для того что бы роутинг работал
 render(
   <BrowserRouter>
     <ErrorBoundary>
       <ThemeProvider>
+        <Button />
         <App />
       </ThemeProvider>
     </ErrorBoundary>
