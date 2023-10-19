61b61a731b88964ffa399cb325d9e90777646ba9
mishasvb777 <mishasvb777@gmail.com>
2023-10-19 19:16:14 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,5 +1,6 @@
 import { fireEvent, screen } from '@testing-library/react'
 import { withTranslation } from 'react-i18next';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import  Sidebar  from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
@@ -7,12 +8,12 @@ import  Sidebar  from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 describe('Sidebar', () => { 
   test('with only first param', () => {
     const SidebarWithTranslation = withTranslation()(Sidebar)
-    renderWithTranslation(<SidebarWithTranslation />);
+    componentRender(<SidebarWithTranslation />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   }) 
 
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />) // здесь мы получаем отрендеренный компонент
+    componentRender(<Sidebar />) // здесь мы получаем отрендеренный компонент
     const toggleBtn = screen.getByTestId('sidebar-toggle') // здесь находим кнопку у которой data-testid="sidebar-toggle"
     expect(screen.getByTestId('sidebar')).toBeInTheDocument(); // проверка, что элемент с тестовым идентификатором ‘sidebar’ существует в документе
     fireEvent.click(toggleBtn) // имитируется нажатие на кнопку с идентификатором “toggleBtn”
