02f2a996c71d20ba40f5d7c5b67f38ba2d71f77e
mishasvb777 <mishasvb777@gmail.com>
2023-10-22 12:58:39 +0300

    add husky precommit

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import  LoginForm from './LoginForm';
+
+const meta = {
+  title: 'features/LoginForm',
+  component: LoginForm,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    
+  },
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {    
+    
+    
+  },
+};
+
+
+
+
